20356e5ed035258fb3b46f83f97e09d8f6ac1f09
Joeyy Hayfron <joeacq81@gmail.com>
2020-11-04 02:59:25 +0000

    Updates

---
+++ precache-manifest.5479d63d2718e40c07ebed96f9c2eceb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1459d637c825b1a3ac867baba4ac0377",
+    "url": "/open-forum/index.html"
+  },
+  {
+    "revision": "8aeb60c5acd7f080d9a0",
+    "url": "/open-forum/static/css/main.1b5b8397.chunk.css"
+  },
+  {
+    "revision": "517f53901e920b91526c",
+    "url": "/open-forum/static/js/2.0383fa03.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/open-forum/static/js/2.0383fa03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aeb60c5acd7f080d9a0",
+    "url": "/open-forum/static/js/main.60db7129.chunk.js"
+  },
+  {
+    "revision": "51c146dd14e3f0f441d4",
+    "url": "/open-forum/static/js/runtime-main.9f00dd63.js"
+  },
+  {
+    "revision": "ce0c8188520a88e43c81e611847721ac",
+    "url": "/open-forum/static/media/facebook.ce0c8188.svg"
+  }
+]);
